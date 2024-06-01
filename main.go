3ee9e6d795622bb01681decc16e199f07c7aa8de
aFoxpl42 <jonskid44@gmail.com>
2024-06-01 10:22:58 +0200

    Added and initially tested getInput function

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func getInput(prompt string, r* bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+	input, err := r.ReadString('\n')
+	return strings.TrimSpace(input), err
+}
+
 
 func main() {
-	fmt.Println("Hello, world!")
+	reader := bufio.NewReader(os.Stdin)
+	prompt := "pokdex > "
+	testInput, _ := getInput(prompt, reader)
+	fmt.Println(testInput)
 }
